d3ad4285e3f9b62447ddbcb3e99b620de552b3f2
Qiu <a2393868407@sina.com>
2018-03-25 10:16:34 +0800

    none

--- Eng/scripts/99 bukkit.js
+++
@@ -1,4 +0,0 @@
-﻿HackFunctions.setItemArrayOffset(HackFunctions.OFFSET_ARMOR);
-HackFunctions.SetItemDefaults(10,205,0);
-HackFunctions.setItemStack(10,99);
-HackFunctions.setItemArrayOffset(HackFunctions.OFFSET_INV);
